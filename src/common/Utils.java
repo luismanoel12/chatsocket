a15a2149f0f32e365cd7a818ecd7c12e3adf1787
luismanoel12 <36317156+luismanoel12@users.noreply.github.com>
2020-04-22 13:43:22 -0300

    projeto

---
+++ src/common/Utils.java
@@ -0,0 +1,39 @@
+package common;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+/**
+ *
+ * @author luism
+ */
+public class Utils {
+    public static boolean sendMessage(Socket sock, String message)
+    {     
+        try {
+            ObjectOutputStream output = new ObjectOutputStream(sock.getOutputStream());
+            output.flush();
+            output.writeObject(message);
+            return true;
+        } catch (IOException ex) {
+            System.err.println("[ERROR:sendMessage] -> " + ex.getMessage());
+        }
+        return false;
+    }
+    
+    public static String receiveMessage(Socket sock)
+    {
+        String response = null;
+        try {
+            ObjectInputStream input = new ObjectInputStream(sock.getInputStream());
+            response = (String) input.readObject();
+        } catch (IOException ex) {
+            System.err.println("[ERROR:receiveMessage] -> " + ex.getMessage());
+        } catch (ClassNotFoundException ex) {
+            System.err.println("[ERROR:receiveMessage] -> " + ex.getMessage());
+        }
+         return response;
+    }
+}
